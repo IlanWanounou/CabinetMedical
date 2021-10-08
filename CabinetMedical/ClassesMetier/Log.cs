5829711044d54a550a49258a88db90bab95999ac
Ilan <wanounouilan@gmail.com>
2021-10-08 19:13:38 +0200

    Ajout méthode GetExceptionJson()

---
+++ CabinetMedical/ClassesMetier/Log.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Log.cs" company="Ilan">
+// Copyright (c) Ilan. All rights reserved.
+// </copyright>
+
+namespace CabinetMedical.ClassesMetier
+{
+    using System;
+
+    /// <summary>
+    /// Class Log.
+    /// </summary>
+    public class Log
+    {
+        private string application;
+        private string className;
+        private DateTime date;
+        private string message;
+        private string user;
+        private string machine;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Log"/> class.
+        /// </summary>
+        /// <param name="message">Message de l'erreur.</param>
+        public Log(string message)
+        {
+            this.application = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name.ToString();
+            this.className = this.GetType().Name.ToString();
+            this.date = DateTime.Now;
+            this.message = message;
+            this.user = Environment.UserName;
+            this.machine = Environment.MachineName;
+        }
+    }
+}
--- CabinetMedical/Exceptions/CabinetMedicalException.cs
+++ CabinetMedical/Exceptions/CabinetMedicalException.cs
@@ -5,7 +5,8 @@
 namespace CabinetMedical.Exceptions
 {
     using System;
-    using System.Reflection;
+    using System.Text.Json;
+    using CabinetMedical.ClassesMetier;
 
     /// <summary>
     /// Class CabinetMedicalException qui hérite de Exception.
@@ -19,19 +20,18 @@ namespace CabinetMedical.Exceptions
         public CabinetMedicalException(string exception)
             : base(exception)
         {
-            TempException tempException = new TempException();
+        }
 
-            var methodInfo = MethodBase.GetCurrentMethod();
-            var fullName = methodInfo.DeclaringType.FullName + "." + methodInfo.Name;
-            tempException.Application = Assembly.GetExecutingAssembly().GetName().Name;
-            tempException.ClasseException = fullName;
-            tempException.DateException = DateTime.Now;
-            tempException.MessageException = exception;
-            tempException.UserExpcetion = Environment.UserName;
-            tempException.UserMachine = Environment.MachineName;
-
-            // Json j = new Json(tempException.Application, tempException.ClasseException, tempException.DateException, tempException.MessageException, tempException.UserMachine, tempException.UserMachine);
-            // j.addError();
+        /// <summary>
+        /// Formate sous forme de JSON.
+        /// </summary>
+        /// <param name="log">Objet log.</param>
+        /// <returns>Format JSON.</returns>
+        public string GetExceptionJson(Log log)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(log, options);
+            return jsonString;
         }
     }
 }
